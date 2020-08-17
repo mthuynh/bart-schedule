8a1c89c49cf863509964a7e9e03f54e2d765976b
Michael Huynh <michaelthuynh@gmail.com>
2020-08-16 20:45:06 -0700

    Updates

---
+++ precache-manifest.44175e9b7f4472b819dcc92bf4600e6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f0b414fa586b996d5e0e0184311ced",
+    "url": "/bart-schedule/index.html"
+  },
+  {
+    "revision": "11dfb095e166942c72db",
+    "url": "/bart-schedule/static/css/main.8a6ba471.chunk.css"
+  },
+  {
+    "revision": "2b5e33aa139b86018d07",
+    "url": "/bart-schedule/static/js/2.6489fc0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bart-schedule/static/js/2.6489fc0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11dfb095e166942c72db",
+    "url": "/bart-schedule/static/js/main.de5e1578.chunk.js"
+  },
+  {
+    "revision": "19f17e7d5ec0b5c2ea78",
+    "url": "/bart-schedule/static/js/runtime-main.b748994d.js"
+  }
+]);
